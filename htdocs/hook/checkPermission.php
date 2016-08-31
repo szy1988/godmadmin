f1117f2ee63548ebb1d179dac6d71fc0b023f7f6
szy1988 <songzeyu1988@gmail.com>
2016-08-31 10:57:54 +0800

    Revert "d"
    This reverts commit cd9e5fed246ba78af74a8229ab163b613f4cd180.

--- htdocs/hook/checkPermission.php
+++
@@ -1,9 +0,0 @@
-<?php
-class checkPermission extends hook{
-
-	public function hook($module,$action,$func,$type=''){
-		//echo 'i\'m hook!<br/>';
-	}
-
-}
-?>
--- htdocs/tmp/tpl_c/%%A4^A46^A463A168%%footer.html.php
+++
@@ -1,9 +0,0 @@
-<?php /* Smarty version 2.6.22, created on 2016-03-31 16:36:21
-         compiled from ./include/footer.html */ ?>
-<div class="footer">©2015 Tencent IEG  |  联系我们：RTX  rebercca</div>
-<script type="text/javascript" src="http://ossweb-img.qq.com/images/script/com/qq/basic/basic.js"></script>
-<script src="http://x.ied.com/static/js/lib/jquery.placeholder.js"></script>
-<script src="http://x.ied.com/static/js/core.js"></script>
-<script type="text/javascript" src="./static/js/common.js"></script>
-</body>
-</html>
